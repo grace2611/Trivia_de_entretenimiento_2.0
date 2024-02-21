28c006b2b415fe756f16c18c4f82104e30a12370
grace2611 <uturuncoramirezgrace@gmail.com>
2024-02-21 16:21:49 -0500

    first commit

--- assets/js/segundo.js
+++ assets/js/segundo.js
@@ -1,17 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const nombre = localStorage.getItem('nombre');
-    const saludoElement = document.getElementById('saludo');
-  
-    if (nombre) {
-      saludoElement.innerText = `Bienvenido(a) ${nombre}!`;
-    } else {
-      saludoElement.innerText = 'Bienvenido(a)!';
-    }
-  });
-  
-  function guardarNombre() {
-    const nombre = document.getElementById('nombre').value;
-    localStorage.setItem('nombre', nombre);
-    window.location.href = 'segundo.html'; 
+  const nombre = localStorage.getItem('nombre');
+  const saludo1Element = document.getElementById('saludo1');
+  const saludo2Element = document.getElementById('saludo2');
+
+  if (nombre) {
+    saludo1Element.innerText = 'Bienvenido(a)';
+    saludo2Element.innerText = `${nombre}!`;
+  } else {
+    saludo1Element.innerText = 'Bienvenido(a)!';
+    saludo2Element.innerText = '';
   }
-  
+
+  const text1Animation = saludo1Element.animate(
+    [
+      { transform: 'translateX(0%) scale(0.9)', opacity: 1 },
+      { transform: 'translateX(0%) scale(1)', opacity: 1 },
+      { transform: 'translateX(0%) scale(1)', opacity: 1 }
+    ],
+    { duration: 1000, fill: 'forwards' }
+  );
+
+  text1Animation.onfinish = () => {
+    const text2Animation = saludo2Element.animate(
+      [
+        { transform: 'translateX(-50%) scale(1)', opacity: 0 },
+        { transform: 'translateX(0%) scale(1)', opacity: 1 }
+      ],
+      { duration: 990, fill: 'forwards' }
+    );
+  };
+});
+
+function guardarNombre() {
+  const nombre = document.getElementById('nombre').value;
+  localStorage.setItem('nombre', nombre);
+  window.location.href = 'segundo.html'; 
+}
